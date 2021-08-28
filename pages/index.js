5894c789e012babd5e53d85c76df394e9a518bae
mikeytown19 <mikeytown19@gmail.com>
2021-08-28 12:01:04 -0600

    3 files modified, 1 added

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 import Head from 'next/head';
-
+import Button from '../components/Button/index.js';
+console.log(Button);
 export default function Home() {
-  return <div>hey</div>;
+  return (
+    <main>
+      <div>hey</div>
+      <Button>Example Button</Button>
+    </main>
+  );
 }
